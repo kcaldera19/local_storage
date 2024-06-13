57290425e7abbd4cfe2a201d6a0a675d7664e86a
Katalina Caldera <katalinacaldera19@gmail.com>
2024-06-13 13:00:35 -0500

    added the js for the index page and did the html for the info page

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,35 @@
 "use strict"
-window.onload=()=>{
+window.onload = () => {
     console.log("connecting to index page")
+    // get a hold of the button
+    document.querySelector("#theButton").addEventListener("click", ()=>{
+
+        const name = document.getElementById('name').value;
+
+
+        const role = document.querySelector("#radioId:checked").value;
+
+        displayInfo(name, role);
+    });
+
+
+
+
+
+
+
+
+}
+
+function displayInfo(name, role) {
+    localStorage.setItem('name', name);
+    localStorage.setItem('role', role);
+
+    if (name && role) {
+
+        window.location.href = "info_page.html";
+
+    }
+
+
 }
