eb25fbd18f211510c137f078565889665429a17b
Katalina Caldera <katalinacaldera19@gmail.com>
2024-06-13 13:23:36 -0500

    added the js for the infos page

--- scripts/info_page.js
+++ scripts/info_page.js
@@ -2,4 +2,21 @@
 
 window.onload=()=>{
     console.log("connected to infos page")
+    const name = localStorage.getItem('name');
+    const role = localStorage.getItem('role');
+
+    if(role === 'instructor'){
+        document.getElementById('welcome').innerText = `Welcome Instructor ${name}`;
+
+        document.getElementById('loremText').innerText = 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...';
+
+        document.getElementById('loremText').style.color ='blue';
+    }else if(role === 'student'){
+        document.getElementById('welcome').innerText = `Welcome Student ${name}`;
+
+        document.getElementById('loremText').innerText = 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...';
+
+        document.getElementById('loremText').style.color = 'purple';
+    }
+
 }
